d76f7f8a6034f2edfe3246d86e48c88d317dda28
sameer0075 <sameerbutt151@gmail.com>
2022-12-29 16:41:15 +0500

    add book form done with formik

--- src/Components/Books/addBook.js
+++ src/Components/Books/addBook.js
@@ -3,6 +3,8 @@ import React,{useState,useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import { connect } from "react-redux";
 import {addBook,authorsList} from '../../Redux/actions/action'
+import { Formik } from 'formik';
+
 function AddBook(props) {
     let navigate = useNavigate()
     let [book_name,setBookName] = useState('')
@@ -34,20 +36,61 @@ function AddBook(props) {
     return (
         <div style={{margin:"5%"}}>
             <h1>Add Book</h1>
-            <form>
+            <Formik
+       initialValues={{ book_name, description , author }}
+       validate={values => {
+        console.log("values",values)
+         const errors = {};
+         if (values.book_name == '') {
+           errors.book_name = 'Required';
+         } else if (
+            values.description == ''
+         ) {
+           errors.description = 'Required';
+         } else if (
+            values.author == ''
+         ) {
+           errors.author = 'Required';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+            console.log("values",values)
+            let {book_name,description,author} = values
+            let data = {
+                book_name,
+                description,
+                author_id:author
+            }
+            props.addBook(data,navigate)
+       }}
+     >
+        {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+             <form onSubmit={handleSubmit}>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4 col-md-6 col-sm-12">
                             <div className="mb-3">
                                 <label for="exampleInputEmail1" className="form-label">Book Name</label>
-                                <input onChange={(e)=>setBookName(e.target.value)} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <input name='book_name' onChange={handleChange} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                {errors.book_name && touched.book_name && errors.book_name}
                             </div>
                         </div>
 
                         <div className="col-lg-4 col-md-6 col-sm-12">
                             <div className="mb-3">
                                 <label for="exampleInputEmail1" className="form-label">Description</label>
-                                <input onChange={(e)=>setDescription(e.target.value)} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <input name='description' onChange={handleChange} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                {errors.description && touched.description && errors.description}
                             </div>
                         </div>
 
@@ -61,7 +104,7 @@ function AddBook(props) {
                         <div className="col-lg-4 col-md-6 col-sm-12">
                             <div className="mb-3">
                                 <label for="exampleInputEmail1" className="form-label">Author</label>
-                                <select onChange={(e)=>setAuthor(e.target.value)} className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
+                                <select name='author' onChange={handleChange} className="form-select form-select-md mb-3" aria-label=".form-select-lg example">
                                     <option>Select Author</option>
                                     {
                                         props.users && props.users.map((user)=>{
@@ -69,6 +112,7 @@ function AddBook(props) {
                                         })
                                     }
                                 </select>
+                                {errors.author && touched.author && errors.author}
                             </div>
                         </div>
                     </div>
@@ -76,7 +120,7 @@ function AddBook(props) {
                     <div className="row">
                         <div className="col">
                             <div className="mb-3">
-                                <button disabled={isDisabled()} onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
+                                <button onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
                                 &nbsp;<button onClick={()=>navigate('/home')} className="btn btn-primary">Back</button>
                             </div>
                         </div>
@@ -84,6 +128,9 @@ function AddBook(props) {
 
                 </div>
             </form>
+       )}
+     </Formik>
+           
         </div>
     )
 }
